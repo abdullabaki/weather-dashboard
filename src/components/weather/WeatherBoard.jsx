9901a1f10ad852e45898fd6d01f4978596671c17
MD. Abdulla Hale Baki <bakimd77@gmail.com>
2025-06-10 02:40:29 +0600

    up

--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -1,15 +1,28 @@
-import AddToFavourite from "./AddToFavourite";
-import WeatherCondition from "./WeatherCondition";
-import WeatherHeadline from "./WeatherHeadline";
+import AddToFavourite from "./AddToFavourite.jsx";
+import WeatherCondition from "./WeatherCondition.jsx";
+import WeatherHeadline from "./WeatherHeadline.jsx";
+
+import { useContext } from "react";
+import { WeatherContext } from "../../context";
 
 export default function WeatherBoard() {
+   const { loading } = useContext(WeatherContext);
+
    return (
       <div className="container">
          <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
             <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-               <AddToFavourite />
-               <WeatherHeadline />
-               <WeatherCondition />
+               {loading.state ? (
+                  <div className="col-span-2 text-center">
+                     <p className="text-lg font-semibold">{loading?.message}</p>
+                  </div>
+               ) : (
+                  <>
+                     <AddToFavourite />
+                     <WeatherHeadline />
+                     <WeatherCondition />
+                  </>
+               )}
             </div>
          </div>
       </div>
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,5 +1,99 @@
-import {useState} from "react";
+import { useEffect, useState } from "react";
 
-const useWeather=()=>{
-    const [weatherData, setweatherData]= useState();
-}
+const useWeather = () => {
+   const [weatherData, setweatherData] = useState({
+      location: "",
+      climate: "",
+      temperature: "",
+      maxTemperature: "",
+      minTemperature: "",
+      humidity: "",
+      cloudPercentage: "",
+      wind: "",
+      time: "",
+      longitude: "",
+      latitude: "",
+   });
+
+   const [loading, setLoading] = useState({
+      state: false,
+      message: "",
+   });
+
+   const [error, setError] = useState(null);
+
+   const fetchWeatherData = async (longitude, latitude) => {
+      try {
+         setLoading({
+            ...loading,
+            state: true,
+            message: "Fetching Weather data",
+         });
+
+         const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${
+               import.meta.env.VITE_WEATHER_API_KEY
+            }&units=metric`
+         );
+
+         if (!response.ok) {
+            const errorMessage = `Fetching weather data failed: ${response.status}`;
+            throw new Error(errorMessage);
+         }
+
+         const data = await response.json();
+
+         const updateWeatherData = {
+            ...weatherData,
+            location: data?.name,
+            climate: data?.weather[0]?.main,
+            temperature: data?.main?.temp,
+            maxTemperature: data?.main?.temp_max,
+            minTemperature: data?.main?.temp_min,
+            humidity: data?.main?.humidity,
+            cloudPercentage: data?.clouds?.all,
+            wind: data?.wind?.speed,
+            time: data?.dt,
+            longitude: longitude,
+            latitude: latitude,
+         };
+         setweatherData(updateWeatherData);
+      } catch (err) {
+         setError(err);
+      } finally {
+         setLoading({
+            ...loading,
+            state: false,
+            message: "",
+         });
+      }
+   };
+
+   useEffect(() => {
+      setLoading({
+         state: true,
+         message: "Finding Location...",
+      });
+
+      navigator.geolocation.getCurrentPosition(function (position) {
+         const lat = position.coords.latitude;
+         const lon = position.coords.longitude;
+
+         // Validation
+         if (lat < -90 || lat > 90 || lon < -180 || lon > 180) {
+            setError(new Error("Invalid coordinates received."));
+            return;
+         }
+
+         fetchWeatherData(lon, lat); // ঠিক order: longitude, latitude
+      });
+   }, []);
+
+   return {
+      weatherData,
+      error,
+      loading,
+   };
+};
+
+export default useWeather;
